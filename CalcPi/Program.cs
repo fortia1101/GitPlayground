001a5fe65f8af784f4c9e5b036adcc2989989b32
ritto <masa00.vr3@gmail.com>
2023-12-02 21:37:24 +0900

    Create C# project 'CalcPi'

---
+++ CalcPi/Program.cs
@@ -0,0 +1,23 @@
+﻿double pi = CalculatePi();
+Console.WriteLine($"円周率の値は {pi} です。");
+
+static double CalculatePi()
+    {
+        double pi = 0;
+        int iterations = 1000000;
+
+        for (int i = 0; i < iterations; i++)
+        {
+            double term = 1.0 / (2 * i + 1);
+            if (i % 2 == 0)
+            {
+                pi += term;
+            }
+            else
+            {
+                pi -= term;
+            }
+        }
+
+        return pi * 4;
+    }
